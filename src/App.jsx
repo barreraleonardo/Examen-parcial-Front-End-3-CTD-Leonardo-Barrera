df41385ffe8d431b8e50ff05e7e0cce66091ef4e
Leonardo Barrera <leonardobarrera397@gmail.com>
2023-05-31 21:31:01 -0300

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Carga de Estudiantes</h1>
+        <form>
+          <input type="text" />
+        </form>
+        <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
